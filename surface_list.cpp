148ff10d3945820aea69e9ed88d63cf93af82d03
ads <adam@cangjie.info>
2015-09-28 00:12:03 -0400

    More work on surface lists.

--- surface_list.cpp
+++ surface_list.cpp
@@ -1,9 +1,18 @@
 #include "surface_list.h"
 #include <QDebug>
 
+int SurfaceList::count = 0;
+
 SurfaceList::SurfaceList()
    : index(-1)
 {
+   count++;
+   listName = QString("list%1").arg(count);
+}
+
+void SurfaceList::setName(QString newName)
+{
+   listName = newName;
 }
 
 void SurfaceList::moveLast()
@@ -11,9 +20,15 @@ void SurfaceList::moveLast()
    index = size() - 1;
 }
 
+void SurfaceList::moveFirst()
+{
+   if(isEmpty()) index = -1;
+   else index = 0;
+}
+
 void SurfaceList::displayList()
 {
-   qDebug() << "SURFACES LIST";
+   qDebug() << QString("SURFACES LIST: %1").arg(listName);
    for(int i = 0; i < size(); i++)
    {
       qDebug() << at(i);
